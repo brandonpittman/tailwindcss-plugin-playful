331b8af29673df7382fb567c28d3486add80ae33
Brandon Pittman <brandon@brandonpittman.net>
2019-12-26 13:44:29 +0900

    wip

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 module.exports = {
   config: {
-    theme: {
-      delight: theme => ({
-        background: theme('colors.teal.500'),
-        foreground: '#fff',
-        soft: 'rgba(255, 255, 255, 0.2)'
-      })
-    }
+    ...require('../tailwindcss-plugin-content').config
   },
   handler (bagOfCrap) {
+    require('../tailwindcss-plugin-content').handler(bagOfCrap)
     require('./lib/buttons/slide')(bagOfCrap)
     require('./lib/spinners/donut')(bagOfCrap)
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
   variants: {
+    display: ['responsive', 'hover', 'focus', 'group-hover']
   },
   plugins: [
-    require('tailwindcss-plugin-content'),
     require('./index')
   ]
 }
